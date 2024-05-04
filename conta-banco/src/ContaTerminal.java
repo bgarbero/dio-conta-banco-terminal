43f6c8bd681fd165416004d64876fb5bb738cc66
bgarbero <bruno_garbero@hotmail.com>
2024-05-04 06:01:48 -0300

    Código do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da agência!");
+        agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o número da conta!");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o seu nome!");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o seu Saldo!");
+        saldo = scanner.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque", nomeCliente, agencia, numero, saldo);
+
+        scanner.close();
     }
 }
